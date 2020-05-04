0445134f40fde36beaad5ddee4800abfdaa4b485
derickfan <fanderick9999@gmail.com>
2020-05-04 01:52:43 -0400

    added little testing

--- src/test/java/dev/derickfan/project2/Project2ApplicationTests.java
+++ src/test/java/dev/derickfan/project2/Project2ApplicationTests.java
@@ -1,13 +1,20 @@
 package dev.derickfan.project2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Project2ApplicationTests {
 
+	@Autowired
+	private MainController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
+		assertThat(controller).isNotNull();
 	}
 
 }
